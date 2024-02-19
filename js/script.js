f6f1ad50e6a0151976c83cb56e5d4491255c10ea
Anna Spjut <anna.spjut@gmail.com>
2024-02-19 18:48:14 +0400

    [add] console result

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log('esercizio "password generator"');
+
+//1.
+const firstName = prompt('Inserisci il tuo nome');
+console.log(firstName);
+
+//2.
+const lastName = prompt('Inserisci il tuo cognome');
+console.log(lastName);
+
+//3.
+const color = prompt('Quale è il tuo colore preferito?');
+console.log(color);
+
+//4.
+const password = firstName + lastName + color + '24'
+
